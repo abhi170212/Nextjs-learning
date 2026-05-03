3e8ed9650a27d9cad61bded827eaf36e18520374
Abhishek Kumar Singh <ram07abhishek@gmail.com>
2026-05-04 00:40:45 +0530

    route group added as ->> (marketing)

---
+++ basic/app/(marketing)/about/page.tsx
@@ -0,0 +1,3 @@
+export default function userAboutPage(){
+     return <h1> This is the About page</h1>
+}
---
+++ basic/app/(marketing)/information/page.tsx
@@ -0,0 +1,3 @@
+export default function userInfoPage(){
+     return <h1> This is the About page</h1>
+}
